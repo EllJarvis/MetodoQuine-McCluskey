d0b2d5934ad2cbaf4aab0c7b88f2e66236234d0c
EllJarvis <javier.r.hernandez.2000@gmail.com>
2024-04-16 07:41:52 -0600

    Inicio del codigo

---
+++ js_sf/contTabla.js
@@ -0,0 +1,31 @@
+(function ($) {
+
+    /**
+     * @param states array with toggle states in format {text: string, active: boolean}
+     */
+    $.fn.nToggle = function (states) {
+        var toggle = this;
+        var count = states.length;
+        var stateWidth = 100 / count;
+        for (var i = 0; i < count; i++) {
+            toggle.append(make(states[i], stateWidth));
+        }
+        
+        return function() {
+            return toggle.children('.active').text();
+        };
+    };
+
+    function make(state, width) {
+        var elem = $('<span class="toggle-elem">');
+        elem.width(width + '%');
+        elem.text(state.text);
+        if (state.active) elem.addClass('active');
+        elem.on('click', function () {
+            elem.parent().children().removeClass('active');
+            elem.addClass('active');
+        });
+        return elem;
+    }
+
+})(jQuery);
